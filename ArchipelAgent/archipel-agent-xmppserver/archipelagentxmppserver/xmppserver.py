f3bf54793afe0863b7f4ecc6c606c33a433cf7c7
Cyril Peponnet <cyril@peponnet.fr>
2013-04-24 12:48:29 +0200

    Added Missing import

--- ArchipelAgent/archipel-agent-xmppserver/archipelagentxmppserver/xmppserver.py
+++ ArchipelAgent/archipel-agent-xmppserver/archipelagentxmppserver/xmppserver.py
@@ -20,6 +20,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import xmpp
+import xmlrpclib
 
 from archipel.archipelHypervisor import TNArchipelHypervisor
 from archipel.archipelVirtualMachine import TNArchipelVirtualMachine
